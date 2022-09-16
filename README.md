6f4a9c7fefdb06c9638b78f0fa2c0f1bd3a27ffb
jun-isaac <100373800+jun-isaac@users.noreply.github.com>
2022-09-16 17:41:35 +0900

    코인 계산 기능 추가, read me 파일 수정

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # react-coin-tracker
+
+
+-----------------
+## 🧑🏻‍💻 구현 목표
+
+- [x] 로딩중에는 Laoding이라는 텍스트가 나오고 로딩이 되면 coin 목록이 뜹니다.
+- [x] api 기능을 활용해 coin json 목록을 불러 옵니다.
+- [x] select 기능을 활용해 coin 선택을 할 수 있습니다.
+- [x] 달러 -> 코인 / 코인 -> 달러 가격 비교를 할 수 있습니다.
+
+-----------------
+## 메인프레임
+
+<img src="images/main.jpg" width="850">
+
+간단하게 api로 json 목록을 불러와서 coin 목록을 띄웁니다.
+
+------------------
+## 💻 메인페이지
+
+<img src="images/coins.gif" width="850">
+
+### 코인 목록
+- select 기능으로 코인을 선택할 수 있습니다.
+
+
+<img src="images/calculation.gif" width="850">
+
+### 계산
+- 달러를 입력하면 코인으로, 코인을 입력하면 달러로 계산해줍니다.
+
+------------------
+## 📕 배운 것
+
+* fetch 기능을 활용해 json 목록을 불러올 수 있다.
+- https://api.coinpaprika.com/v1/tickers / api 기능을 활용해 json을 fetch로 코드에 넣었다.
+
+
+<img src="images/learn.jpg" width="850">
+
+* json 내부에 다양한 item을 활용 가능하다.
+- const [coins, setCoins] = useState ([]); / coins라는 Array를 만들어서 setcoins 함수를 활용해 fetch로 json을 coins로 변경해주면 json 내부에 다양한 item을 활용할 수 있다.
+
+* {xxx ? "" : ""} = {xxx ? 만약 xxx가 true라면 동작 : 만약 xxx가 false 라면 동작}
+- if를 활용해서 loading 하는 동안 다른 화면을 띄울수 있다.
+
+
+------------------
+## 🔨 수정해야 할 것
+
+
+### ❌ 보기 좋게 CSS 수정
+
+
+## 사용 라이브러리
+|React|
+|---|
--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
 import { useEffect, useState } from "react";
+import React from 'react';
 
+function USDtoCoins({ prices }) {
+  const [amount, setAmount] = React.useState("");
+  const [inverted, setInverted] = React.useState(false);
+  const onChange = (event) => {
+    // console.log(event.target.value);
+    setAmount(event.target.value);
+    console.log(amount);
+  };
+
+  const reset = () => setAmount("");
+  const onInvert = () => {
+    reset();
+    setInverted((current) => !current);
+  };
+
+  return (
+
+    <div>
+      <div>
+        <label htmlFor="usd">USD</label>
+        <input
+          value={inverted ? amount * prices : amount}
+          id="usd"
+          placeholder="0"
+          type="number"
+          onChange={onChange}
+          disabled={inverted}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="coin">Coins</label>
+        <input
+          value={inverted ? amount : amount / prices}
+          id="coin"
+          placeholder="0"
+          type="number"
+          onChange={onChange}
+          disabled={!inverted}
+        />
+      </div>
+      <button onClick={reset}>Reset</button>
+      <button onClick={onInvert}>{inverted ? "USD" : "Coins"}</button>
+    </div>
+
+  );
+}
+
+// ! 1. App이 실행 된다.
 function App() {
   const [loading, setLoading] = useState (true);
   const [coins, setCoins] = useState ([]);
+  const [price, setPrice] = useState(0);
+  const onSelect = (event) => {
+    setPrice(event.target.value)
+    // console.log(event.target.value);
+    // price = event.target.value
+    // console.log(price);
+  }
+
   useEffect(() => {
+    // ! 3. fetch를 활용해 json 항목을 불러와 준다.
     fetch("https://api.coinpaprika.com/v1/tickers")
+    // ! 4. .then(( response 객체는 fetch를 통해서 요청했을 때 웹서버가 응답한 결과를 담고있는 객체데이터이며, 객체 안에는 여러가지의 속성값을 통해서 서버와 통신 시 어떠한 상태로 통신이 이루워져있는지 알 수 있는 다양한 단서들이 들어있다.) => response.json / response에서 json 파일을 불러온다.())
     .then((response) => response.json())
     .then((json) => {
+      // ! 5. 불러온 json 목록을 setCoins 함수를 사용해 coins라는 Array에 넣어준다.
       setCoins(json);
+      // ! 6. setLoading 함수를 사용해 loading을 false로 변경해준다.
       setLoading(false);
     }
     );
   }, []);
+  // console.log(coins)
+
+  // ! 2. {loading ? 만약 loading이 true라면 Loading... 띄우고 : 아니라면 코인 목록을 불러온다. {coins.map / Array안의 모든 item을 수정 가능한 함수. 즉 위에 설정한 coins라는 Array에 coin을 넣어준다. ((coin의 정보를 가지고 온다. / 아무 이름 지정 가능, 원하면 banana라고 해도 되는데 그럴 경우에는 return을 banana.name등으로 작성해야 한다.) => <option>{coin의 이름을 불러온다.} ({coin의 symbol을 가지고 온다.}): {coin > quotes > USD > price를 가지고 온다.} USD</option>)}}
   return (
     <div>
       <h1>The Coins! {loading ?  "" : `(${coins.length})`}</h1>
+      
       {loading ? <strong>Loading...</strong> :
-            <select>
+            <select onChange={onSelect}>
+              <option value="xx">Select Coins</option>
             {coins.map((coin) =>
-            <option>{coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD</option>)}
+            <option key={coin.id} value={(coin.quotes.USD.price)}>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD</option>)}
           </select>
       }
+
+      {price === 0 ? null : price === "xx" ? null : <USDtoCoins prices={price} />}
     </div>
   );
 }
